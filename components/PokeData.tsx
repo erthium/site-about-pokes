f2d7d3c81919e5a2fcf3448013c4b2e79e324878
Erthium <ertyum35@gmail.com>
2024-01-29 11:28:13 +0300

    Start the pokeData style process and fill general parts

--- components/PokeData.tsx
+++ components/PokeData.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../styles/pokedata.module.css';
 
 export interface PokeDataProps {
   name: string;
@@ -13,29 +14,29 @@ export interface PokeDataProps {
 
 const PokeData: React.FC<PokeDataProps> = ({name, imageUrl, pokeTypes, stats, abilities, abilityDefs, moves, moveDefs}) => {
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={imageUrl} alt={name}/>
-      <h3>Types</h3>
-      <ul>
+    <div className={styles.container}>
+      <h2 className={styles.pokeName}>{name}</h2>
+      <img className={styles.pokeImage} src={imageUrl} alt={name}/>
+      <h3 className={styles.title}>Types</h3>
+      <ul className={styles.listContainer}>
         {pokeTypes.map((type, index) => (
-          <li key={index}>{type}</li>
+          <li className={styles.listItem} key={index}>{type}</li>
         ))}
       </ul>
-      <h3>Stats</h3>
-      <h4>{stats}</h4>
-      <h3>Abilities and Definitions</h3>
-      <ul>
+      <h3 className={styles.title}>Stats</h3>
+      <h4 className={styles.normalText}>{stats}</h4>
+      <h3 className={styles.title}>Abilities and Definitions</h3>
+      <ul className={styles.listContainer}>
         {abilities.map((ability, index) => (
-          <li key={index}>
+          <li className={styles.listItem} key={index}>
             {ability} - {abilityDefs[index]}
           </li>
         ))}
       </ul>
-      <h3>Moves and Definitions</h3>
-      <ul>
+      <h3 className={styles.title}>Moves and Definitions</h3>
+      <ul className={styles.listContainer}>
         {moves.map((move, index) => (
-          <li key={index}>
+          <li className={styles.listItem} key={index}>
             {move} - {moveDefs[index]}
           </li>
         ))}
