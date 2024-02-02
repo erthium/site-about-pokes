09b166e18f1f8318ea33a2a8b88740882f5f8b6c
Erthium <ertyum35@gmail.com>
2024-02-02 16:16:37 +0300

    Change the env variables from server, just hard-code it

--- services/pokeApiService.ts
+++ services/pokeApiService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://facts-about-pokes.vercel.app';
 
 const fetchData = async (endpoing: string, parameter: any) => {
     try {
